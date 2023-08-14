24378cbdfbe4321288cc7b5f280a1ee7d828e7f9
Kathyw15 <kathytw@berkeley.edu>
2023-08-13 19:52:15 -0700

    add gallery page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: './index.html',
         about: './about.html',
+        gallery: './gallery.html',
         // ...
         // List all files you want in your build
       }
